98b609bf2c401f93503223b12858e954c86361b0
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-07-05 16:00:26 +0600

    wipe

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn1');
+const navbar = document.getElementById('navbar');
+
+btn.addEventListener('click', function onClick() {
+    navbar.style.backgroundColor = '#00FFFF';
+
+
+});
