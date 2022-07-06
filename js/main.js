b7038dee8704585949f441e64e0244ae6f77a52e
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-07-06 14:53:14 +0600

    wip

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('btn1');
 const navbar = document.getElementById('navbar');
 
 btn.addEventListener('click', function onClick() {
-    navbar.style.backgroundColor = '#00FFFF';
+    navbar.style.backgroundColor = '#2596be';
 
 
 });
